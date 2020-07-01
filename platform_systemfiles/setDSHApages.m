9c56ac9adcb70ec6b419a9624435888b6c25f076
Gabriel Candia <gacandia@gmail.com>
2020-07-01 09:29:13 -0400

    Update
    Update

--- platform_systemfiles/setDSHApages.m
+++
@@ -1,9 +0,0 @@
-function[pages]=setDSHApages(Nscen)
-
-nMax   = min(Nscen,20);
-pages  = unique([1:nMax:Nscen,Nscen])';
-if numel(pages)==1
-    pages = [1 1];
-else
-    pages  = [pages(1:end-1),pages(2:end)-1];pages(end)=Nscen;
-end
